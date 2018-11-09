5fa6745fd98ea86c2b39bde8a904ce89a09c9176
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-11-09 20:29:00 +0000

    receiving json data

--- routes/clients.js
+++ routes/clients.js
@@ -1,11 +1,20 @@
 const express         = require("express");
 const router          = express.Router();
+const axios           = require('axios');
 
 module.exports = () => {
 
-   router.get("/shoppers", (req, res) => {
+   router.get("/info", (req, res) => {
+     axios.get(`http://interview.mapsted.com/RnD/test-buildings.json`)
+     .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
      res.render("clients")
    });
-
    return router;
 }
