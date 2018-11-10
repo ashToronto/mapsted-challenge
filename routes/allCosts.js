00d9d14f8f00569f1caa75d354ef046196c416f2
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-11-10 05:53:24 +0000

    added some css for welcome page

--- routes/allCosts.js
+++ routes/allCosts.js
@@ -1,34 +1,3 @@
-const express = require("express");
-const router = express.Router();
-const axios = require('axios');
-
-module.exports = () => {
-
-let index_id = [];
-let item_total_cost = [];
-
-
-// Helper function Sum all costs from array
-function add(a, b) {
-    return a + b;
-}
-
-function indexOfMax(arr) {
-    if (arr.length === 0) {
-        return -1;
-    }
-    var max = arr[0];
-    var maxIndex = 0;
-    for (var i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            maxIndex = i;
-            max = arr[i];
-        }
-    }
-
-    return maxIndex+1;
-}
-
 // Again, we make 2 api calls
 // Looping over all data from building api and saving each building_id in an array
 // Loop over analytics api and match key value with item_id
@@ -36,32 +5,62 @@ function indexOfMax(arr) {
 // save summed value to summary array
 // find index of greatest value in summary array
 
-// Total purchases for Samsung Manufacturers
+
+const express = require("express");
+const router = express.Router();
+const axios = require('axios');
+
+module.exports = () => {
+
+  let index_id = [];
+  let item_total_cost = [];
+
+
+  // Helper function Sum all costs from array
+  function add(a, b) {
+    return a + b;
+  }
+
+  function indexOfMax(arr) {
+    if (arr.length === 0) {
+      return -1;
+    }
+    var max = arr[0];
+    var maxIndex = 0;
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        maxIndex = i;
+        max = arr[i];
+      }
+    }
+
+    return maxIndex + 1;
+  }
+
+  // Total purchases for Samsung Manufacturers
   router.get("/info", (req, res) => {
     axios.get(`http://interview.mapsted.com/RnD/test-buildings.json`)
       .then(function(response) {
         // Loop through data object until purchase costs arrays - Tripple nested for loop approach
-        for (i in response.data){
-        index_id.push(response.data[i].building_id)
-      }
-        // console.log("us_items_id: ", us_items_id)
+        for (i in response.data) {
+          index_id.push(response.data[i].building_id)
+        }
       })
       // Chain to our second api call
       .then(
         axios.get(`http://interview.mapsted.com/RnD/test-analytics.json`)
-        .then(function(result){
-          // console.log("RESULT ", result.data)
-          for (d in index_id){
+        .then(function(result) {
+          for (d in index_id) {
             let temporary = [];
-            for (i in result.data){
-                for (j in result.data[i].usage_statistics.session_infos){
-                  const placeholder = result.data[i].usage_statistics.session_infos[j]
-                  if (placeholder.building_id === index_id[d]){
-                    for (k in placeholder.purchases){
-                      temporary.push(placeholder.purchases[k].cost)
-                    }
+            for (i in result.data) {
+              for (j in result.data[i].usage_statistics.session_infos) {
+                const placeholder = result.data[i].usage_statistics.session_infos[j]
+                if (placeholder.building_id === index_id[d]) {
+                  for (k in placeholder.purchases) {
+                    temporary.push(placeholder.purchases[k].cost)
                   }
                 }
+              }
             }
             let total = temporary.reduce(add, 0)
             item_total_cost.push(total)
@@ -69,13 +68,15 @@ function indexOfMax(arr) {
           }
           console.log("TOTAL: ", item_total_cost)
           const greatest_cost = indexOfMax(item_total_cost)
-          const templateVars = {data: greatest_cost}
+          const templateVars = {
+            data: greatest_cost
+          }
           // Math.max.apply(Math,item_total_cost)
           console.log("Q5 ANSWER : " + greatest_cost)
           res.render("all_costs", templateVars)
           item_total_cost = [];
         })
       )
-});
+  });
   return router;
 }
